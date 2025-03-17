62cbb185ecae6d003097425821f4324142f033f8
idoco <ido@loadmill.com>
2025-03-17 16:51:26 +0200

    add more action

--- actions.js
+++ actions.js
@@ -5,6 +5,28 @@ export async function handleModelAction(page, action) {
         console.log(`Clicking at (${action.x}, ${action.y})`);
         await page.mouse.click(action.x, action.y);
         break;
+      case "double_click":
+        console.log(`Double clicking at (${action.x}, ${action.y})`);
+        await page.mouse.dblclick(action.x, action.y);
+        break;
+      case "move":
+        console.log(`Moving mouse to (${action.x}, ${action.y})`);
+        await page.mouse.move(action.x, action.y);
+        break;
+      case "drag":
+        console.log("Dragging along path", action.path);
+        if (Array.isArray(action.path) && action.path.length > 0) {
+          const [firstPoint, ...restPoints] = action.path;
+          await page.mouse.move(firstPoint.x, firstPoint.y);
+          await page.mouse.down();
+          for (const point of restPoints) {
+            await page.mouse.move(point.x, point.y);
+          }
+          await page.mouse.up();
+        } else {
+          console.log("Drag action missing a valid path");
+        }
+        break;
       case "scroll":
         console.log(`Scrolling by (${action.scrollX}, ${action.scrollY})`);
         await page.mouse.wheel(action.scrollX, action.scrollY);
@@ -24,6 +46,18 @@ export async function handleModelAction(page, action) {
       case "wait":
         console.log("Waiting...");
         await page.waitForTimeout(2000);
+        break;
+      case "goto":
+        console.log(`Navigating to ${action.url}`);
+        await page.goto(action.url);
+        break;
+      case "back":
+        console.log("Navigating back");
+        await page.goBack();
+        break;
+      case "forward":
+        console.log("Navigating forward");
+        await page.goForward();
         break;
       case "screenshot":
         console.log("Screenshot action received - no execution needed.");
