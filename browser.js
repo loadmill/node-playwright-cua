ad68a446745f69835f1409cb894c0f475685a982
idoco <ido@loadmill.com>
2025-03-17 16:39:25 +0200

    browser config

--- browser.js
+++ browser.js
@@ -1,15 +1,18 @@
 import { chromium } from "playwright";
 
 export async function launchBrowser() {
+  const width = 1024;
+  const height = 768;
+
   const browser = await chromium.launch({
     headless: false,
     chromiumSandbox: true,
-    env: {},
-    args: ["--disable-extensions", "--disable-file-system"],
+    env: { DISPLAY: ":0" },
+    args: [`--window-size=${width},${height}`, "--disable-extensions", "--disable-file-system"],
   });
 
   const page = await browser.newPage();
-  await page.setViewportSize({ width: 1024, height: 768 });
+  await page.setViewportSize({ width, height });
 
   return { browser, page };
 }
